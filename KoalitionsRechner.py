e4f71a41fbb45e171e8a4f2e9fc95b3f8398393b
EinGuterWaran <Janusan@Lingeswaran.de>
2021-09-27 23:46:27 +0200

    Eingabe nun in Sitzen + 5% Hürde nun überflüssig

--- KoalitionsRechner.py
+++ KoalitionsRechner.py
@@ -6,34 +6,33 @@ import numpy as np
 # Output: Alle rein rechnerischen Koalitionsmöglichkeiten welche eine absolute Mehrheit ergeben würden
 # English: All coalition combinations that would have an absolute majority
 
-## Input
+## Instesamt Sitze
+sitze = 735
+# Anzahl Sitze pro Partei
 er = [
-        ["SPD", 25.7],
-        ["CDU/CSU", 24.1],
-        ["GRÜNE", 14.8],
-        ["FDP", 11.5],
-        ["AfD", 10.3],
-        ["DIE LINKE", 4.9]
+        ["SPD", 206],
+        ["CDU/CSU", 196],
+        ["GRÜNE", 118],
+        ["FDP", 92],
+        ["AfD", 83],
+        ["DIE LINKE", 39]
     ]
-## End Input
+#Sitzanzahl wird in Prozent umgerechnet
+for el in er:
+    el[1] = round(el[1]/sitze*100,5)
 
+#Hilfsfunktion welche Kombinationen der Länge r von Parteien zurückgibt
 def rSubset(arr, r):
       return list(combinations(arr, r))
-      
-if __name__ == "__main__":
-    #5% Hürde
-    for party in er: 
-        if party[1] < 5:
-            er.remove(party)
 
+if __name__ == "__main__":
     coalInd = 1
     partyCombinations = []
     for x in range (len(er), 0, -1):
         partyCombinations.extend(rSubset(er, x))
     for partyCombination in partyCombinations:
         partyComb = np.array(partyCombination)
-        
-        voteSum = sum(partyComb[:, 1].astype(np.float))
+        voteSum = sum(partyComb[:, 1].astype(float))
         if voteSum > 50:
             print("#"+str(coalInd)+": "+', '.join(str(x) for x in partyComb[:, 0]) +" mit "+ str(round(voteSum,2))+ "%.")
             coalInd += 1
